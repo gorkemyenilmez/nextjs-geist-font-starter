004e4a1a75af6ab31750b1f6956ba4d98f0fbbe5
gorkemyenilmez <yenilmezr0@gmail.com>
2025-07-21 11:34:10 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753086866535

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_bcc5850a._.js",
+  "static/chunks/node_modules_3b446d55._.js"
+],
+    source: "dynamic"
+});
